8be7dd18297da4d03cfe4e6a80ca3437d1401c8c
mohammed <mohammedabdelbasit500@gmail.com>
2023-09-07 02:32:39 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
-#include <unistd.h>
+
 /**
  * main - Entry point
  *
- * discripiton: using sizeof to print the varios types
+ * discripiton: using sizeof to print the varios types size_t write(int fd, const void *buf, size_t count);
  *
- * Return: Always 0 (success)
+ * Return: 1 (error).
 */
 
 int main(void)
 {
-char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
-writ(1, quo, 59);
+char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+write(1, quo, 59);
 return (1);
 }
