b17812e9df8d31222f92d75d509411bd63543c28
mohammed <mohammedabdelbasit500@gmail.com>
2023-09-07 01:10:43 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * discripiton: using sizeof to print the varios types
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+writ(1, quo, 59)
+return (0);
+}
