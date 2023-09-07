04cf019962d7bd985260f0f5e5f5ab61936bfa36
mohammed <mohammedabdelbasit500@gmail.com>
2023-09-06 23:38:24 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* discription: A c program that print the value of puts function
+*
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0 ; }
