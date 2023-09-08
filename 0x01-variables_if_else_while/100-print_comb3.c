56c9e7d83de93c116ae4d2943091b306702080bc
mohammed <mohammedabdelbasit500@gmail.com>
2023-09-08 05:12:37 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,11 +10,13 @@
 
 int main(void)
 {
-int digit1 = 0, digit2;
+int digit1 = 0;
+int digit2;
 while (digit1 <= 9)
 {
 digit2 = 0;
-while (digit2 <= 9)
+while (digit2 <= 9) 
+{
 if (digit1 != digit2 && digit1 < digit2)
 {
 putchar(digit1 + 48);
