19894ce658ad20a82337f5a2d3b3687c566c8bd2
mohammed <mohammedabdelbasit500@gmail.com>
2023-09-08 01:15:31 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * discripiton: print the decimal number
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+int num;
+for (num = 0; num < 10; num++)
+{
+printf("%d", num);
+
+return (0);
+}
