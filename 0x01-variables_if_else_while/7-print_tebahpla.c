32856534d65162286b48d7bc7a6f07c8a19717cb
mohammed <mohammedabdelbasit500@gmail.com>
2023-09-08 01:30:41 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * discripiton: print the alphabets in reverse case
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+char cr = 'z';
+while (cr >= 'a')
+{
+putchar(cr\n);
+cr--;
+
+return (0);
+}
