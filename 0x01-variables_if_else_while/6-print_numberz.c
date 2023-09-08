5d0bd35c1f8e04e81d903b2106d550d1f03de0ae
mohammed <mohammedabdelbasit500@gmail.com>
2023-09-08 01:20:08 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * discripiton: print the decimal number by usiin putchar
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+int i = 0;
+while (i < 10)
+{
+putchar(i);
+i++
+}
+
+return (0);
+}
