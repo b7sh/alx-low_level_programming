f0a051f92b4f7a461fa8f32b3b567872da1f186d
mohammed <mohammedabdelbasit500@gmail.com>
2023-09-08 01:42:50 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * discripiton: print the hexadecimal in lower case
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+int number = 48;
+while (number <= 102)
+{
+putchar(number);
+if (number == 57)
+digit += 39;
+digit++;
+}
+putchar('\n');
+
+return (0);
+}
+
