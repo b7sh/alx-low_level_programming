5fb549ac3e655877cc78b222c3182432bf11348e
mohammed <mohammedabdelbasit500@gmail.com>
2023-09-08 00:37:25 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * discripiton: print alphapets in lower case
+ *
+ * Return: Always 0 (success)
+*/
+
+#include <stdio.h>
+int main(void)
+{
+char chr = 'a';
+
+while (chr <= 'z')
+{
+putchar(chr);
+chr++;
+}
+
+}
