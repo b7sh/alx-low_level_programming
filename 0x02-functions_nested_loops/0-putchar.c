0eb5be0ed038c5d4bda9815f76fcb7a25cc81a6a
mohammed <mohammedabdelbasit500@gmail.com>
2023-09-12 03:19:31 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,16 +8,12 @@
  * Return: Always 0 (success)
 */
 
-int main(void)
+int main(void);
 {
-	_putchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
-	_putchar("\n");
+	char str[] = "_putchar"
+		int ch;
+	for (ch = 0; ch < 8; ch++)
+		_puchar(str[ch]);
+	_putchar('\n');
 	return (0);
 }
