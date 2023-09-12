dd4b982ecd29fccab3e93372ea0869a04dad3b73
mohammed <mohammedabdelbasit500@gmail.com>
2023-09-12 05:24:45 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - function that add tow integers and return to the result
+* @bro: the first integer will add to sis
+* @sis: the second integer will add to bro
+* Return: add tow numbrs
+*/
+
+int add(int bro, int sis)
+{
+int me = bro + sis;
+return (me);
+}
