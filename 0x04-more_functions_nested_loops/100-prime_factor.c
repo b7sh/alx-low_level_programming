d50aacd49bf08b52165a41f0867c43535ea6adce
mohammed <mohammedabdelbasit500@gmail.com>
2023-09-14 14:54:54 -0700

    100-prime_factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -10,30 +10,30 @@
 
 int main(void)
 {
-	long int a;
-	long int mx;
-	long int b;
+long int a;
+long int mx;
+long int b;
 
 a = 612852475143;
 mx = -1;
 
 while (a % 2 == 0)
 {
-	mx = 2;
-	a /= 2;
+mx = 2;
+a /= 2;
 }
 for (b = 3; b <= sqrt(a); b = b + 2)
 {
-	while (a % b == 0)
-	{
-		mx = b;
-	a = a / b;
-	}
+while (a % b == 0)
+{
+mx = b;
+a = a / b;
+}
 }
 if (a > 2)
 {
-	mx = a;
-	printf("%ld", mx);
+mx = a;
+printf("%ld", mx);
 }
-	return (0);
+return (0);
 }
