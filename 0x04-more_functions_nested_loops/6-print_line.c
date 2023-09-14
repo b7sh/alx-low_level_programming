8b41e53c0ed321ae04d815576a62c2a0160f2159
mohammed <mohammedabdelbasit500@gmail.com>
2023-09-14 11:42:38 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - draw a straight line in the termina
+ *
+ *@n: integer for print
+ *
+ * Return: always 0 (success)
+ */
+
+
+void print_line(int n)
+{
+int line;
+if (n <= 0)
+_putchar('\n');
+else
+{
+for (line = 1; line < n; line++)
+_putchar('_');
+_putchar('\n');
+}
+}
