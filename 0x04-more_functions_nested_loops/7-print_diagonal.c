37675500e5ba212e9c3fd7b899d8b32a6128f129
mohammed <mohammedabdelbasit500@gmail.com>
2023-09-16 07:26:47 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,24 +9,23 @@
 
 void print_diagonal(int n)
 {
-
 if (n <= 0)
 {
-_putchar('\n');
+putchar('\n');
 }
 else
 {
 int i, j;
-for (i = 0; i < n; i++)
+for (i = 0; i < 11; i++)
 {
-for (j = 0; j < n; j++)
+for (j = 0; j <= i; j++)
 {
-if j == i
-_putchar('\\');
+if (j == i)
+putchar(92);
 else if (j < i)
-_putchar(' ');
+putchar(' ');
 }
-_putchar('\n');
+putchar('\n');
 }
 }
 }
