e56d58d66f90309010c4180236d0f761f387c3e5
mohammed <mohammedabdelbasit500@gmail.com>
2023-09-18 06:46:42 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _puts - a function that prints a string, followed 
+ * _puts - a function that prints a string, followed
  * by a new line, to stdout
  * @str: the character we use
  */
