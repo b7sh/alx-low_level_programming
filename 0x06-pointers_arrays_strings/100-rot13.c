1ac37830cc891a5b2be8274c8bb8259d01adfc2b
mohammed <mohammedabdelbasit500@gmail.com>
2023-09-21 13:52:08 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *rot13 - a function that encodes
+ * a string using rot13.
+ * @m: the pointer character
+ * Return: m
+ */
+
+char *rot13(char *m)
+{
+	int i;
+
+	for (i = 0; m[i] != '\0'; i++)
+	{
+		if (m[i] >= 'a' && m[i] <= 'm' || m[i] >= 'A' && m[i] <= 'Z')
+		{
+			m[i] += 13;
+		}
+		else if (m[i] >= 'n' && m[i] <= 'z'|| m[i] >= 'N' && m[i] <= 'Z')
+		{
+			m[i] -= 13;
+		}
+	}
+	return (m);
+}
