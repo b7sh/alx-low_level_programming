6313c97929eb1cef5173da0b1fbb704c58b52ac9
mohammed <mohammedabdelbasit500@gmail.com>
2023-09-21 16:44:20 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -15,9 +15,9 @@ void print_number(int n)
 		_putchar('-');
 		sign = -n;
 	}
-	if (n / 10 != 0)
+	if (sign / 10 != 0)
 	{
-		print_number(n / 10);
+		print_number(sign / 10);
 	}
-	_putchar('0' + (n % 10));
+	_putchar('0' + (sign % 10));
 }
