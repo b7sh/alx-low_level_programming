bd24b76a23b62bf5db9c9d47173d507020a4c472
mohammed <mohammedabdelbasit500@gmail.com>
2023-09-20 18:09:41 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * *cap_string - a function that capitalizes
+ * all words of a string
+ * @s: the pointer to retutn
+ * Return: s
+ */
+
+char *cap_string(char *s)
+{
+	int cap = 1, i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == ' ' || s[i] == '\n' || s[i] == '\t' || s[i] == '.'
+				|| s[i] == '-')
+		{
+			cap = 1;
+		}
+		else if (cap)
+		{
+			if (s[i] >= 'a' && s[i] <= 'z')
+			{
+				s[i] = s[i] - ('a' - 'A');
+			}
+		cap = 0;
+		}
+	}
+	return (s);
+}
