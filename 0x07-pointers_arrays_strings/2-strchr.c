448657074f158deb622b3420ddfda57e90723b39
mohammed <mohammedabdelbasit500@gmail.com>
2023-09-25 15:56:31 -0700

    all

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,14 +10,14 @@
 
 char *_strchr(char *s, char c)
 {
-int i = 0;
+	int i = 0;
 
-do {
-	i++;
-	if (s[i] == c)
-	{
-		return (s + i);
-	}
-} while (s[i] >= '\0');
-return ('\0');
+	do {
+		i++;
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	} while (s[i] >= '\0');
+	return ('\0');
 }
