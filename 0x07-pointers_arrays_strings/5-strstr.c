42f2e3840bed6c7ec3832f8ac93a795b840a97a7
mohammed <mohammedabdelbasit500@gmail.com>
2023-09-25 17:05:17 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strstr - function locates a substring
+ * @haystack: the main pointer
+ * @needle: the compare pointer
+ * Return: haystack
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	for (;*haystack != '\0'; haystack++)
+	{
+		char *a = haystack;
+		char *b = needle;
+
+		while (*a == *b && *b != '\0')
+		{
+			a++;
+			b++;
+		}
+		if (*b == '\0')
+		{
+			return (haystack);
+		}
+	}
+	return (0);
+}
