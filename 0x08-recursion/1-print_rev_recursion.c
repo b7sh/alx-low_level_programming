b2446065781206115f12f8f91d06a56b0fa20202
boshy <boshy500@gmail.com>
2023-09-28 22:08:52 +0300

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _print_rev_recursion - print the string in reverse
+* @s: the charactar
+*/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+}
+
