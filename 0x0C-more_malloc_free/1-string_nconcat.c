552521a868a203dd283de8515d6cbbc4916ecc52
mohammed <mohammedabdelbasit500@gmail.com>
2023-10-07 13:27:17 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -26,7 +26,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; s1[i] != '\0'; i++)
 		s3[i] = s1[i];
 	for (j = 0; j < n && s2[j] != '\0'; j++)
-		s3[i++] = s2[j];
+	{
+		s3[i] = s2[j];
+		i++;
+	}
 	s3[i] = '\0';
 	return (s3);
 }
