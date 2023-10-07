f574dedad7c872b9e385b38616a6bacf22958458
mohammed <mohammedabdelbasit500@gmail.com>
2023-10-06 18:03:30 -0700

    100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,62 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * strlen - function to find the lenghth of a string
+ * @p: the charactar
+ * Return: k
+ */
+
+int strln(char *p)
+{
+	int k = 0;
+
+	while (*p != '\0')
+	{
+		k++;
+		p++;
+	}
+	return (k);
+}
+
+/**
+ * *argstostr - a function to concatenate the argument
+ * @ac: hold the value of the program
+ * @av: hold the string
+ * Return: null or s
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int sum = 0;
+	int i1, i2, j;
+	char *s;
+
+	j = 0;
+
+	if (ac == 0 || av == 0)
+	{
+		return (NULL);
+	}
+	for (i1 = 0; i1 < ac; i1++)
+	{
+		sum += strln(av[i1]);
+		sum++;
+	}
+	s = malloc(sizeof(char) * sum + 1);
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (i1 = 0; i1 < ac; i1++)
+	{
+		for (i2 = 0; av[i1][i2] != '\0'; i2++)
+		{
+			s[j] = av[i1][i2];
+			j++;
+		}
+		s[j] = '\n';
+		j++;
+	}
+	return (s);
+}
