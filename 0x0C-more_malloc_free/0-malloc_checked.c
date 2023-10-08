14b7f2c7a5812ef846ce9d881d3f0c5da105eb5c
mohammed <mohammedabdelbasit500@gmail.com>
2023-10-08 15:49:13 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -11,14 +11,9 @@ void *malloc_checked(unsigned int b)
 {
 	int *s;
 
-<<<<<<< HEAD
-        s = malloc(b);
-
-=======
 	s = malloc(b);
 	
->>>>>>> e63869df8b3325bff2cf49626af359dd4cc61634
-	if (s == NULL)
+	while (s == NULL)
 	{
 		exit(98);
 	}
