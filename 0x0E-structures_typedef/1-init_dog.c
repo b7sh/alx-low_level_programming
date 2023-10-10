044cee248acc0fc2790d6369941ab06afaf11c49
mohammed <mohammedabdelbasit500@gmail.com>
2023-10-10 12:49:51 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,7 +9,7 @@
  * @owner: the second string
  */
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d->name = name;
 	d->age = age;
