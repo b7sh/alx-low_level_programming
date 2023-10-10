644c6b051cc2871d60ca5ab0281cdb5204eff82c
mohammed <mohammedabdelbasit500@gmail.com>
2023-10-10 12:27:27 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * dog - the header file
+ * @name: the first pointer
+ * @age: the float variable
+ * @owner: the second pointer
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
