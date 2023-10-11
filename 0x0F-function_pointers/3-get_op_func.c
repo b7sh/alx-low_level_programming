b35f94d21486278735237afc8ff9eeb77312fc4c
mohammed <mohammedabdelbasit500@gmail.com>
2023-10-11 03:31:02 -0700

    3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -21,7 +21,7 @@ int (*get_op_func(char *s))(int, int)
 	i = 0;
 	while (ops[i].op != NULL)
 	{
-		if (strcmp((ops[i].op), s) == 0)
+		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
 		i++;
 	}
