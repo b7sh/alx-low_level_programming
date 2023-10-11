82ebfd97532959e95de75f3556e10c01f7ec484c
mohammed <mohammedabdelbasit500@gmail.com>
2023-10-11 02:47:57 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,6 +11,8 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t index;
 
+	if (!array || !action)
+		return;
 	for (index = 0; index < size; index++)
 		action(array[index]);
 }
