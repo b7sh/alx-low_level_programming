8d63fec16d5607a18b60a4f1e83c831b05577360
mohammed <mohammedabdelbasit500@gmail.com>
2023-10-19 15:05:42 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_list - print the total nodes
+ * @h: the head
+ * Return: result
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t result = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0](nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		result++;
+	}
+	return (result);
+}
