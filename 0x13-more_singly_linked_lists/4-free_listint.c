0410198f0fa951354ba5c6ebeb188b1e8d1d0860
mohammed <mohammedabdelbasit500@gmail.com>
2023-10-24 01:13:19 -0700

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint - free the temporary memory
+ * @head: the head of the list
+*/
+
+void free_listint(listint_t *head)
+{
+	listint_t *lcr;
+
+	while (head != NULL)
+	{
+		lcr = head->next;
+		free(head->next);
+		free(head);
+		head = lcr;
+	}
+}
