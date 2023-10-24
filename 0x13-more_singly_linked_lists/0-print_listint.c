2a2090ec641caf7c0a6b39ca55f3f1f180f43f56
mohammed <mohammedabdelbasit500@gmail.com>
2023-10-24 00:24:30 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_listint - print the list element
+ * @h: the list head
+ * Return: count
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
