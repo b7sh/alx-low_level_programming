906b64c4397402144af9f361ab62e73f89051111
mohammed <mohammedabdelbasit500@gmail.com>
2023-10-30 12:10:43 -0700

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -71,8 +71,8 @@ void display_elf_header(const char *filename)
 	printf("  Class:	%d\n", (header.e_ident[EI_CLASS] == ELFCLASS32)
 		? 32 : 64);
 	printf("  Data:	%s\n", (header.e_ident[EI_DATA] == ELFDATA2LSB)
-		? "2's complement, little-endian"
-		: "2's complement, big-endian");
+		? "2's complement, little endian"
+		: "2's complement, big endian");
 	printf("  Version: %d\n", header.e_ident[EI_VERSION]);
 	printf("  OS/ABI:	%d\n", header.e_ident[EI_OSABI]);
 	printf("  ABI Version: %s\n", get_str(header.e_ident[EI_ABIVERSION]));
