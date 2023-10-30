e7b21ae001ce88f2cf3d31b85695f549d93b6a89
mohammed <mohammedabdelbasit500@gmail.com>
2023-10-30 08:35:54 -0700

    100-elf_header.c

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,61 @@
+#include "main.h"
+/**
+ * main - entry point
+ * @argc: the number of argument
+ * @argv: an array pointer to argument
+ * Return: always 0 (success)
+*/
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
+		exit(98);
+	}
+	display_elf_heasder(argv[1]);
+	return (0);
+}
+/**
+ * display_elf_header - print the elf header content
+ * @filename: hold the file
+*/
+void display_elf_header(const char *filename)
+{
+	int fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		print_error_and_exit("Error: can't open file\n");
+	Elf32_Ehdr header;
+
+	if (read(fd, &header, sizeof(Elf32_Ehdr)) != sizeof(Elf32_Ehdr))
+		print_error_and_exit("Error: can't read ELF header\n");
+	if (header.e_ident[EI_MAGO] != ELFMAGO)
+		print_error_and_exit("Error: Not an ELF file");
+	if (header.e_ident[EIMAG1] != ELFMAG1)
+		print_error_and_exit("Error: Not an ELF file");
+	if (header.e_ident[EIMAG2] != ELFMAG2)
+		print_error_and_exit("Error: Not an ELF file");
+	if (header.e_ident[EI_MAG3] != ELFMAG3)
+		print_error_and_exit("Error: Not an ELF file");
+	printf("Magic: %c\n", header.e_ident[EI_MAG1]);
+	printf("Magic: %c\n", header.e_ident[EI_MAG2]);
+	printf("Magic: %c\n", header.e_ident[EI_MAG3]);
+	printf("Magic: %c\n", header.e_ident[EI_MAG0]);
+	printf("Class: %d-bit\n", (header.e_ident[EI_CLASS] == ELFCLASS32) ? 32 : 64);
+	printf("Data: %s\n", (header.e_ident[EI_DATA] == EFDATA2LSB));
+	printf("Version: %d\n", header.e_ident[EI_VERSION]);
+	printf("OS/ABI: %d\n", header.e_ident[EI_OSPI]);
+	printf("ABI Version: %d\n", header.e_ident[EI_ABIVERSION]);
+	printf("Type: %d\n", header.e_type);
+	printf("Entry point address: 0x%x\n", header.e_entry);
+	close(fd);
+}
+/**
+ * print_error_and_exit - printing the error message and exit
+ * @message: hold the error message
+*/
+void print_error_and_exit(const char *message)
+{
+	fprintf(stderr, "%s\n", message);
+	exit(98);
+}
