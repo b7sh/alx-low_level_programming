2a63f9f1d320f2f8ac5d5fdf11a676c1465d6293
mohammed <mohammedabdelbasit500@gmail.com>
2023-10-30 05:13:29 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -59,7 +59,7 @@ int main(int argc, char *argv[])
 		exit(98); }
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_to == -1)
-	{	dprintf(STDERR_FILENO, "Error: Can't write to  %s\n", argv[2]);
+	{	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		free(fp);
 		close_the_file(file_from);
 		exit(99); }
