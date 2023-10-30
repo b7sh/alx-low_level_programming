4e38094823b6c6c3783dc3d9932113d8c81ccea8
mohammed <mohammedabdelbasit500@gmail.com>
2023-10-30 04:40:59 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -46,7 +46,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "usage: cp file_from file_to");
+		dprintf(STDERR_FILENO, "usage: cp file_from file_to\n");
 		exit(97); }
 	fp = create_buffer();
 	file_from = open(argv[1], O_RDONLY);
