c057bf6cb456642d8110424d67fbfab6d140bce2
mohammed <mohammedabdelbasit500@gmail.com>
2023-10-30 11:48:54 -0700

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -67,19 +67,19 @@ void display_elf_header(const char *filename)
 			header.e_ident[EI_MAG2],
 			header.e_ident[EI_MAG3],
 			header.e_ident[EI_MAG0]);
-	printf("Class:                             %s\n",
+	printf("Class:                             %d\n",
 		(header.e_ident[EI_CLASS] == ELFCLASS32)
-		? "ELF32" : "ELF64");
+		? 32 : 64);
 	printf("Data:                              %s\n",
 		(header.e_ident[EI_DATA] == ELFDATA2LSB)
 		? "2's complement, little-endian"
 		: "2's complement, big-endian");
 	printf("Version:                           %d\n", header.e_ident[EI_VERSION]);
-	printf("OS/ABI:                            %s\n",
-		get_str(header.e_ident[EI_OSABI]));
-	printf("ABI Version:                       %d\n",
-		header.e_ident[EI_ABIVERSION]);
-	printf("Type:                              %s\n", get_str(header.e_type));
+	printf("OS/ABI:                            %d\n",
+		header.e_ident[EI_OSABI]);
+	printf("ABI Version:                       %s\n",
+		get_str(header.e_ident[EI_ABIVERSION]));
+	printf("Type:                              %d\n", (header.e_type));
 	printf("Entry point address:               0x%x\n", header.e_entry);
 	close(fd);
 }
