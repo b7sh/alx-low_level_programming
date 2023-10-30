77383613acc5a1eeb127a66d67883b134fa82a7b
mohammed <mohammedabdelbasit500@gmail.com>
2023-10-30 11:15:08 -0700

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,5 +1,25 @@
 #include "main.h"
 
+/**
+ * get_str - get the string for the abi version and type
+ * @:abi_version: hold the abi version value
+*/
+
+const char *get_str(uint8_t abi_type)
+{
+	switch (abi_type)
+	{
+		case 0:
+			return "System V";
+		case 1:
+			return "HP-UX";
+		case 255:
+			return "Processor-specific";
+		default:
+		return "Unknown";
+	}
+}
+
 void display_elf_header(const char *filename);
 void print_error_and_exit(const char *message);
 
@@ -41,20 +61,21 @@ void display_elf_header(const char *filename)
 	{
 		print_error_and_exit("Error: Not an ELF file");
 	}
-	printf("Magic: %c %c %c %c\n",
+	printf("ELF Header:\n");
+	printf("Magic:	%c %c %c %c\n",
 			header.e_ident[EI_MAG1],
 			header.e_ident[EI_MAG2],
 			header.e_ident[EI_MAG3],
 			header.e_ident[EI_MAG0]);
-	printf("Class: %d-bit\n", (header.e_ident[EI_CLASS] == ELFCLASS32) ? 32 : 64);
-	printf("Data: %s\n", (header.e_ident[EI_DATA] == ELFDATA2LSB)
+	printf("Class:			%s\n", (header.e_ident[EI_CLASS] == ELFCLASS32) ? "ELF32" : "ELF64");
+	printf("Data:			%s\n", (header.e_ident[EI_DATA] == ELFDATA2LSB)
 		? "2's complement, little-endian"
 		: "2's complement, big-endian");
-	printf("Version: %d\n", header.e_ident[EI_VERSION]);
-	printf("OS/ABI: %d\n", header.e_ident[EI_OSABI]);
-	printf("ABI Version: %d\n", header.e_ident[EI_ABIVERSION]);
-	printf("Type: %d\n", header.e_type);
-	printf("Entry point address: 0x%x\n", header.e_entry);
+	printf("Version:		%d\n", header.e_ident[EI_VERSION]);
+	printf("OS/ABI:			%d\n", header.e_ident[EI_OSABI]);
+	printf("ABI Version:		%s\n", get_str(header.e_ident[EI_ABIVERSION]));
+	printf("Type:			%s\n", get_str(header.e_type));
+	printf("Entry point address:		0x%x\n", header.e_entry);
 	close(fd);
 }
 /**
