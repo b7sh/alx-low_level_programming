9d50c4f6cb3544ea50e5080d19b318ab2c8214a3
b7sh <mohammedabdelbasit500@gmail.com>
2024-02-21 04:54:08 -0800

    5-

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+""" returns the perimeter of the island described in grid """
 
 def island_perimeter(grid):
-
+    """  return the perimeter of island """
 
     width = len(grid[0])
     height = len(grid)
